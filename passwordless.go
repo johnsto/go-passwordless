ddf62d6a4dfe2e6370cf26e7a232f437f8405ecb
Dave Johnston <dave@johnsto.co.uk>
2015-11-23 07:56:40 +0000

    Update Strategy

--- passwordless.go
+++ passwordless.go
@@ -13,14 +13,28 @@ var (
 	ErrNoTransport = errors.New("no transports have been configured")
 )
 
-type strategy struct {
-	t   Transport
-	g   TokenGenerator
+// Strategy defines how to send and what tokens to send to users.
+type Strategy interface {
+	Transport
+	TokenGenerator
+	// TTL should return the time-to-live of generated tokens.
+	TTL(context.Context) time.Duration
+}
+
+// SimpleStrategy combines a Transport, TokenGenerator and TTL into a struct
+// for convenience.
+type SimpleStrategy struct {
+	Transport
+	TokenGenerator
 	ttl time.Duration
 }
 
+func (s SimpleStrategy) TTL(context.Context) time.Duration {
+	return s.ttl
+}
+
 type Passwordless struct {
-	transports map[string]strategy
+	Transports map[string]Strategy
 	store      TokenStore
 	opts       Options
 }
@@ -31,7 +45,7 @@ type Options struct {
 func New(store TokenStore) *Passwordless {
 	return &Passwordless{
 		store:      store,
-		transports: make(map[string]strategy),
+		Transports: make(map[string]Strategy),
 	}
 }
 
@@ -40,10 +54,10 @@ func New(store TokenStore) *Passwordless {
 // are valid. Some delivery mechanisms may require longer TTLs than others
 // depending on the nature/punctuality of the transport.
 func (p *Passwordless) SetTransport(name string, t Transport, g TokenGenerator, ttl time.Duration) {
-	p.transports[name] = strategy{
-		t:   t,
-		g:   g,
-		ttl: ttl,
+	p.Transports[name] = SimpleStrategy{
+		Transport:      t,
+		TokenGenerator: g,
+		ttl:            ttl,
 	}
 }
 
@@ -52,8 +66,8 @@ func (p *Passwordless) SetTransport(name string, t Transport, g TokenGenerator, 
 // want to let the user choose one.
 func (p *Passwordless) ListTransports(ctx context.Context) map[string]Transport {
 	ts := map[string]Transport{}
-	for n, t := range p.transports {
-		ts[n] = t.t
+	for n, t := range p.Transports {
+		ts[n] = t
 	}
 	return ts
 }
@@ -61,16 +75,16 @@ func (p *Passwordless) ListTransports(ctx context.Context) map[string]Transport 
 // GetTransport returns the Transport of the given name, or nil if one does
 // not exist.
 func (p *Passwordless) GetTransport(ctx context.Context, name string) Transport {
-	return p.transports[name].t
+	return p.Transports[name]
 }
 
 // RequestToken generates and delivers a token to the given user.
 func (p *Passwordless) RequestToken(ctx context.Context, s, uid, recipient string) error {
-	st, ok := p.transports[s]
+	st, ok := p.Transports[s]
 	if !ok {
 		return fmt.Errorf("unknown strategy '%s'", s)
 	}
-	return requestToken(ctx, p.store, st.t, st.g, uid, recipient, st.ttl)
+	return requestToken(ctx, p.store, st, st, uid, recipient, st.TTL(ctx))
 }
 
 // VerifyToken verifies the provided token is valid.
